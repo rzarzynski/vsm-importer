37616037901f393b02d75f363099ec396c76d928
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2015-06-29 16:38:20 +0200

    crush_converter.py: add a test code.

--- src/crush_converter.py
+++ src/crush_converter.py
@@ -39,19 +39,54 @@ class LLC(object):
         return True
 
     def add_types(self):
-        pass
+        self._formatter.format_oneline_section('type',
+                ['osd', 'node', 'zone', 'storage_group', 'root'])
+
+    def add_devices(self):
+        self._formatter.format_oneline_section('device',
+                map(lambda item: item['name'], self._osdtree.get_osds()))
+
+    def add_hosts_osds(self):
+        hosts = self._osdtree.get_hosts()
+        names = map(lambda item: item['name'], hosts)
+
+        for entity in itertools.product(names, self._storage_groups,
+                self._zones):
+            entity_name = '_'.join(entity)
+            properties = {}
+            properties['id'] = '-1'
+            properties['alg'] = 'straw'
+            properties['hash'] = '0'
+            properties['item'] = []
+            self._formatter.format_multiline_section('host', entity_name,
+                    properties)
 
     def add_root(self):
         pass
 
     def add_storage_groups(self):
-        pass
+        for storage_group in self._storage_groups:
+            self._formatter.format_multiline_section('storage_group',
+                    storage_group, {})
 
     def add_zones(self):
-        pass
+        for entity in itertools.product(self._zones, self._storage_groups):
+            entity_name = '_'.join(entity)
+            self._formatter.format_multiline_section('zone', entity_name, {})
 
     def add_rulesets(self):
         pass
 
 if __name__ == '__main__':
+    with open('/tmp/hier.txt', 'r') as fh:
+        osdtree = OSDTree(json.loads(fh.read()))
+        formatter = Formatter()
+
+        conv = Converter(osdtree, formatter)
+        conv.add_types()
+        conv.add_devices()
+        conv.add_hosts_osds()
+        conv.add_zones()
+        conv.add_storage_groups()
+        print(formatter.get_content())
     pass

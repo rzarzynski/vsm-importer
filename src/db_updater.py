c8c420805bbeeb9b4c946543da26b780022c4aef
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2015-06-30 13:44:46 +0200

    db_updater.py: add skeleton.
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

---
+++ src/db_updater.py
@@ -0,0 +1,8 @@
+from vsm import db
+from vsm import exception
+from vsm.openstack.common import log as logging
+from vsm import utils
+from vsm import conductor
+
+if __name__ == '__main__':
+    pass

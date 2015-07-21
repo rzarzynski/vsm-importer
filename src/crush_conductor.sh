7dcda1b7a59703ffbec84ff03befb225078501d7
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2015-07-21 18:49:41 +0200

    crush_conductor.sh: set noout and limit rebalance impact.

--- src/crush_conductor.sh
+++ src/crush_conductor.sh
@@ -20,11 +20,16 @@ ${CRUSHTOOL} -d ${TMP_OLD_CRUSH_BIN} -o ${TMP_OLD_CRUSH_TXT}
 ${CEPH} osd tree -f json-pretty -o ${TMP_OSD_TREE_TXT}
 
 # conversion
+${CEPH} osd set noout
+${CEPH} tell osd.\* injectargs '--osd-max-backfills 1'
+${CEPH} tell osd.\* injectargs '--osd-recovery-max-active 1'
+
 echo ${TMP_OSD_TREE_TXT} > ${TMP_NEW_CRUSH_TXT}
 ${CRUSHTOOL} -c ${TMP_NEW_CRUSH_TXT} -o ${TMP_NEW_CRUSH_BIN}
 
 # injecting
 ${CEPH} osd setcrushmap -i ${TMP_NEW_CRUSH_BIN}
+${CEPH} osd unset noout
 
 # cleaning
 rm -f   ${TMP_OSD_TREE_TXT}     \
